c1d4bf757428c24b3254c8ce645e454d368e97cc
codrut51 <codrutsoft13@gmail.com>
2018-04-06 11:05:11 +0200

    Logic working needs to be split in classes

--- ThreadingApplication/Elements/Api.cs
+++ ThreadingApplication/Elements/Api.cs
@@ -1,22 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using ThreadingApplication.Elements;
 
 namespace ThreadingApplication
 {
     class Api
     {
         private String apiString;
-
+        private String function;
+        private String symbol;
+        private String market;
+        private String apiKey;
+        private List<Stock> stocks;
         public Api()
         {
+            apiString = "https://www.alphavantage.co/query?function=";
+            function = "DIGITAL_CURRENCY_DAILY";
+            symbol = "&symbol=BTC";
+            market = "&market=EUR";
+            apiKey = "&apikey=FQWIO3W4ONAZNLGL";
+            apiString = apiString + function + symbol + market + apiKey;
+            stocks = new List<Stock>();
+        }
+        /// <summary>
+        /// This function gets the 
+        /// </summary>
+        /// <param name="from">The cryptocurrency from which you want to see the value</param>
+        /// <param name="to"> The market available stock ex: EUR, RON, GBR (British Pound) </param>
+        /// <returns></returns>
+        public async Task setCryptoDayly(String from, String to)
+        {
+            stocks = new List<Stock>();
+            apiString = "https://www.alphavantage.co/query?function=";
+            function = "DIGITAL_CURRENCY_DAILY";
+            symbol = "&symbol="+from;
+            market = "&market="+to;
+            apiKey = "&apikey=FQWIO3W4ONAZNLGL";
+            apiString = apiString + function + symbol + market + apiKey;
+            List<String> def = new List<String>();
+            def.Add("1a. open ");
+            def.Add("2a. high ");
+            def.Add("3a. low ");
+            def.Add("4a. close ");
+            List<String> modify = new List<String>();
+            foreach(String prop in def)
+            {
+                modify.Add(prop + "(" + to + ")");
+            }
+            using (HttpClient c = new HttpClient())
+            {
+                using (HttpResponseMessage get = await c.GetAsync(apiString))
+                {
+                    using (HttpContent cont = get.Content)
+                    {
+                        String result = await cont.ReadAsStringAsync();
+                        if(result != null)
+                        {
+                            JObject o = JObject.Parse(result);
+                            JObject body = (JObject)o["Time Series (Digital Currency Daily)"];
+                            foreach(JProperty p in body.Properties())
+                            {                                
+                                //Debug.WriteLine("\""+ p.Name + "\"" + " : " + p.Value);
+                                JObject propriety = JObject.Parse(p.Value.ToString());
+                                if (p.Name.Contains("2018"))
+                                {
+
+                                    Stock s = new Stock(p.Name);
+                                    foreach (String prop in modify)
+                                    {
+                                        s.Proprieties.Add(prop, propriety.GetValue(prop).ToString());
+                                        ///Debug.WriteLine(propriety.GetValue(prop));
+                                    }
+                                    this.stocks.Add(s);
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            Debug.WriteLine("There is nothing to be displayed");
+                        }
+                    }
+                }
+            }
         }
 
-        public Chart getChart()
+
+        /// <summary>
+        /// This function gets the 
+        /// </summary>
+        /// <param name="from">The cryptocurrency from which you want to see the value</param>
+        /// <param name="to"> The market available stock ex: EUR, RON, GBR (British Pound) </param>
+        /// <returns></returns>
+        public async Task setCryptoMontly(String from, String to)
         {
-            return null;
+            stocks = new List<Stock>();//DIGITAL_CURRENCY_MONTHLY
+            apiString = "https://www.alphavantage.co/query?function=";
+            function = "DIGITAL_CURRENCY_MONTHLY";
+            symbol = "&symbol=" + from;
+            market = "&market=" + to;
+            apiKey = "&apikey=FQWIO3W4ONAZNLGL";
+            apiString = apiString + function + symbol + market + apiKey;
+            List<String> def = new List<String>();
+            def.Add("1a. open ");
+            def.Add("2a. high ");
+            def.Add("3a. low ");
+            def.Add("4a. close ");
+            List<String> modify = new List<String>();
+            foreach (String prop in def)
+            {
+                modify.Add(prop + "(" + to + ")");
+            }
+            using (HttpClient c = new HttpClient())
+            {
+                using (HttpResponseMessage get = await c.GetAsync(apiString))
+                {
+                    using (HttpContent cont = get.Content)
+                    {
+                        String result = await cont.ReadAsStringAsync();
+                        if (result != null)
+                        {
+                            JObject o = JObject.Parse(result);
+                            JObject body = (JObject)o["Time Series (Digital Currency Monthly)"];
+                            foreach (JProperty p in body.Properties())
+                            {
+                                Debug.WriteLine("\"" + p.Name + "\"" + " : " + p.Value);
+                                JObject propriety = JObject.Parse(p.Value.ToString());
+                                if (p.Name.Contains("2018"))
+                                {
+                                    Stock s = new Stock(p.Name);
+                                    foreach (String prop in modify)
+                                    {
+                                        s.Proprieties.Add(prop, propriety.GetValue(prop).ToString());
+                                        ///Debug.WriteLine(propriety.GetValue(prop));
+                                    }
+                                    this.stocks.Add(s);
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            Debug.WriteLine("There is nothing to be displayed");
+                        }
+                    }
+                }
+            }
+        }
+        public List<Stock> getStocks()
+        {
+            return stocks;
         }
 
         public Notification getNews()
--- ThreadingApplication/MainPage.xaml.cs
+++ ThreadingApplication/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using ThreadingApplication.Elements;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -24,7 +27,35 @@ namespace ThreadingApplication
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            // this.InitializeComponent();
+            displayResult();
+        }
+        public async Task displayResult()
+        {
+            Api api = new Api();
+            await api.setCryptoDayly("BTC", "EUR");
+            Debug.WriteLine("Here");
+            api.getStocks();
+            foreach (Stock s in api.getStocks())
+            {
+                Debug.WriteLine(s.Date + " ");
+                foreach (KeyValuePair<string, string> entry in s.Proprieties)
+                {
+                    Debug.WriteLine(entry.Key + " " + entry.Value);
+                }
+            }
+
+            await api.setCryptoMontly("BTC", "EUR");
+            Debug.WriteLine("Here");
+            api.getStocks();
+            foreach (Stock s in api.getStocks())
+            {
+                Debug.WriteLine(s.Date + " ");
+                foreach (KeyValuePair<string, string> entry in s.Proprieties)
+                {
+                    Debug.WriteLine(entry.Key + " " + entry.Value);
+                }
+            }
         }
     }
 }
