78983bde93bc0b9d2dda31a8acae08043473d4d4
Eric Smith <smith.eric.george@gmail.com>
2018-04-07 13:23:53 +0200

    Database Added

--- ThreadingApplication/Elements/Database.cs
+++ ThreadingApplication/Elements/Database.cs
@@ -3,21 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
 
 namespace ThreadingApplication
 {
     class Database
     {
-        private String conn;
+        private System.Text.EncodingProvider ppp;
+        private string address;
+        private MySqlConnection conn;
+        private MySqlCommand cmd;
 
-        public void createUser(String user, String pass)
+        public Database()
         {
-
+            ppp = System.Text.CodePagesEncodingProvider.Instance;
+            Encoding.RegisterProvider(ppp);
+            address = "server=localhost;uid=root;pwd=;database=threading_db;SslMode=None";
+            conn = new MySqlConnection(address);
+            conn.Open();
+            cmd = conn.CreateCommand();
         }
 
-        public bool checkUser(String user, String pass)
+        public void closeConnection()
         {
-            return false;
+            conn.Close();
+        }
+
+        public void createUser(string email, string user, string password)
+        {
+            try
+            {
+                cmd.CommandText = "INSERT INTO `user`(`email`, `name`, `password`) VALUES ('" + email + "','" + user + "','" + password + "')";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public bool checkUser(string email, string password)
+        {
+            bool success = false;
+            try
+            {
+                cmd.CommandText = "SELECT `password` FROM `user` WHERE `email` = '" + email + "'";
+                MySqlDataReader mySqlDataReader = cmd.ExecuteReader();
+                mySqlDataReader.Read();
+                if (mySqlDataReader.HasRows && mySqlDataReader.GetValue(0).Equals(password))
+                {
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return success;
         }
 
         public void addPreference(Preference p)
